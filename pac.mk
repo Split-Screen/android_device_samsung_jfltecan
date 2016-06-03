03fa14aa895a1c88410ca207ae89cb0f1ba5d76f
Lokesh Chamane <lokesh.chamane@pac-rom.com>
2016-06-02 22:01:09 -0400

    jfltecan: PAC-ify
    Change-Id: I97225ff70157cb64df556299e526fb1dc2df16d1
    Signed-off-by: Lokesh Chamane <lokesh.chamane@pac-rom.com>

--- pac.mk
+++ pac.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/samsung/jfltecan/full_jfltecan.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common PAC stuff.
+$(call inherit-product, vendor/pac/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
   PRODUCT_NAME=jfltevl \
@@ -9,5 +9,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
   BUILD_FINGERPRINT="samsung/jfltevl/jfltecan:4.4.2/KOT49H/I337MVLUFNC1:user/release-keys" \
   PRIVATE_BUILD_DESC="jfltevl-user 4.4.2 KOT49H I337MVLUFNC1 release-keys"
 
-PRODUCT_NAME := cm_jfltecan
+PRODUCT_NAME := pac_jfltecan
 PRODUCT_DEVICE := jfltecan
